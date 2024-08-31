702cdf6335588110596ae2f87a271103166bb288
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-08-30 21:22:51 -0300

    ajustes

--- main.ts
+++ main.ts
@@ -64,23 +64,25 @@ const persistence = new Persistence('session.json', '[]');
 const session = new Session(persistence);
 
 io.on('connection', (socket) => {
-
-    session.addUser(new User(socket.id));
+    session.addUser(new User(socket.id));        
     session.save();
-
 
     console.log('A user connected');
 
     socket.on('MESSAGE', (msg: string) => {
         console.log('Message: ' + msg + '| id:' + socket.id);
-        io.emit('MESSAGE', msg);
+        session.findUser(socket.id)?.addMessage(msg)
+        session.save()
+        io.emit('MESSAGE', { text: msg, client: socket.id });
     });
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
+        session.dropUser(socket.id)
+        session.save()
     });
 });
 
-server.listen(3000, '10.3.32.27', () => {
+server.listen(3000, '10.3.58.27', () => {
     console.log(`Server is running on http://localhost:3000`);
 });
